fd7a26344ec6d9882341d31c0e0aad952e0dfa13
Alexander Bukin <alex.bukin@hotmail.com>
2022-06-13 18:11:39 +0100

    Working on local Passport OAuth

--- client/src/components/Forms/Auth.jsx
+++ client/src/components/Forms/Auth.jsx
@@ -8,34 +8,52 @@ import { lang } from '../assets/Localization/language';
 import authLocalization from '../assets/Localization/authLocalization.json';
 
 class Auth extends React.Component {
-	render() {
-		return (
-			<form className="form" action="/auth" method="post">
-				<Field component={Input} type="text" label={authLocalization[4].authForms.email[lang]} name="email" />
-				<Field component={Input} type="password" label={authLocalization[4].authForms.password[lang]} name="password" />
-				<div className="auth-buttons">
-					<input type="submit" value={authLocalization[0].authButtons.login[lang]} className="form-button" />
-					{
-						authLocalization[1].socialAuthButtons.map(authButton => {
-							return (
-								<a 
-									href={ `/auth/${authButton.name.toLocaleLowerCase()}` }  
-									style={ {backgroundColor: authButton.color} } 
-									className={ `form-button social ${authButton.disabled === true ? `disabled` : ''}` }
-									onClick={ () => this.setState({auth: authButton.name.toLocaleLowerCase()}) }
-								>
-									<i className={ authButton.icon }></i>
-									<span>{ authLocalization[2].text[lang] } { authButton.name }</span>
-								</a>
-							)
-						})
-					}
-				</div>
-			</form>
-		)
-	}
+  render() {
+    return (
+      <form className='form' action='/auth' method='get'>
+        <Field
+          component={Input}
+          type='text'
+          label={authLocalization[4].authForms.email[lang]}
+          name='email'
+        />
+        <Field
+          component={Input}
+          type='password'
+          label={authLocalization[4].authForms.password[lang]}
+          name='password'
+        />
+        <div className='auth-buttons'>
+          <input
+            type='submit'
+            value={authLocalization[0].authButtons.login[lang]}
+            className='form-button'
+          />
+          {authLocalization[1].socialAuthButtons.map((authButton) => {
+            return (
+              <a
+                href={`/auth/${authButton.name.toLocaleLowerCase()}`}
+                style={{ backgroundColor: authButton.color }}
+                className={`form-button social ${
+                  authButton.disabled === true ? `disabled` : ''
+                }`}
+                onClick={() =>
+                  this.setState({ auth: authButton.name.toLocaleLowerCase() })
+                }
+              >
+                <i className={authButton.icon}></i>
+                <span>
+                  {authLocalization[2].text[lang]} {authButton.name}
+                </span>
+              </a>
+            );
+          })}
+        </div>
+      </form>
+    );
+  }
 }
 
-const auth = reduxForm({form: 'auth'})(Auth);
+const auth = reduxForm({ form: 'auth' })(Auth);
 
 export default connect(null)(auth);
