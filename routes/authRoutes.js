36d734e7cfec2a0e9144e88be7aac3c9354aa9ea
Alexander Bukin <alex.bukin@hotmail.com>
2022-06-13 07:46:05 +0100

    Facebook now asking for permission to log in. App now able to maintain a logged in state

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,34 +2,39 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
-module.exports = app => {
-	app.get('/auth/google/', 
-		passport.authenticate('google', { scope: ['profile', 'email'] })
-	);
+module.exports = (app) => {
+  app.get(
+    '/auth/google/',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+  );
 
-	app.get('/auth/google/callback',
-		passport.authenticate('google'), (req, res) => {
-			res.redirect('/');
-		}
-	);
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/');
+    }
+  );
 
-	app.get('/auth/facebook',
-  	passport.authenticate('facebook', { scope: ['user_friends', 'public_profile'] })
-	);
+  app.get(
+    '/auth/facebook',
+    passport.authenticate('facebook', { scope: ['email', 'public_profile'] })
+  );
 
-	app.get('/auth/facebook/callback',
-		passport.authenticate('facebook'), (req, res) => {
-			res.redirect('/');
-		}
-	);
+  app.get(
+    '/auth/facebook/callback',
+    passport.authenticate('facebook'),
+    (req, res) => {
+      res.redirect('/');
+    }
+  );
 
-	app.get('/api/current_user', async (req, res) => {
-		await res.send(req.user);
-	});
+  app.get('/api/current_user', async (req, res) => {
+    await res.send(req.user);
+  });
 
-	app.get("/api/logout", (req, res) => {
-		req.logout();
-		res.redirect("/");
-});
-
-}
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+};
--- services/passport.js
+++ services/passport.js
@@ -4,57 +4,63 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 const keys = require('./keys');
 const mongoose = require('mongoose');
 
-const User = mongoose.model("users");
+const User = mongoose.model('users');
 
-passport.serializeUser(function(user, done) {
-	done(null, user);
+passport.serializeUser(function (user, done) {
+  done(null, user);
 });
-  
+
 passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
+  User.findById(id).then((user) => {
     done(null, user);
   });
 });
 
-passport.use(new GoogleStrategy({
-	clientID: keys.googleClientID,
-	clientSecret: keys.googleClientSecret,
-	callbackURL: '/auth/google/callback'
-},
-	async (accessToken, refreshToken, profile, done) => {
-		console.log(profile);
-		const existingUser = await User.findOne({ userId: profile.id });
-		if (existingUser){
-			return done(null, existingUser);
-		}
-		const user = await new User({
-			userId: profile.id,
-			fullName: profile.displayName,
-			avatar: profile.photos[0].value,
-			provider: profile.provider
-		}).save();
-		done(null, user);
-	}
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+      const existingUser = await User.findOne({ userId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({
+        userId: profile.id,
+        fullName: profile.displayName,
+        avatar: profile.photos[0].value,
+        provider: profile.provider,
+      }).save();
+      done(null, user);
+    }
+  )
+);
 
-passport.use(new FacebookStrategy({
-	clientID: keys.facebookClientID,
-	clientSecret: keys.facebookClientSecret,
-	callbackURL: '/auth/facebook/callback',
-	profileFields: ['id', 'displayName', 'photos', 'email']
-},
-	async (accessToken, refreshToken, profile, done) => {
-		console.log(profile);
-		const existingUser = await User.findOne({ userId: profile.id });
-		if (existingUser){
-			return done(null, existingUser);
-		}
-		const user = await new User({
-			userId: profile.id,
-			fullName: profile.displayName,
-			avatar: profile.photos[0].value,
-			provider: profile.provider
-		}).save();
-		done(null, user);
-	}
-));
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookClientID,
+      clientSecret: keys.facebookClientSecret,
+      callbackURL: '/auth/facebook/callback',
+      profileFields: ['id', 'displayName', 'photos', 'email'],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+      const existingUser = await User.findOne({ userId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({
+        userId: profile.id,
+        fullName: profile.displayName,
+        avatar: profile.photos[0].value,
+        provider: profile.provider,
+      }).save();
+      done(null, user);
+    }
+  )
+);
