748bc6bdb79005bca18c08d9954f18634a7f7ac0
lxndrbukin <alex.bukin@hotmail.com>
2022-07-03 20:10:04 -0700

    Working on the  Profile mockup

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -3,24 +3,52 @@ import './Profile.scss';
 import { connect } from 'react-redux';
 
 class Profile extends React.Component {
+  addName() {
+    return <button>Add Name</button>;
+  }
+
+  addEmail() {
+    return <button>Add Email</button>;
+  }
+
   render() {
     return (
-      <div className='profile'>
+      <div className='profile-wrapper'>
         <div className='profile_header'>Profile</div>
-        <div className='profile-info'>
-          <div className='profile-info_avatar'>
-            <img
-              src={this.props.currentUser.avatar}
-              alt={this.props.currentUser.fullName}
-            />
-          </div>
-          <div className='profile-info_name'>
-            <span>Name:</span>
-            <span>
-              {this.props.currentUser.fullName
-                ? this.props.currentUser.fullName
-                : 'Loading'}
-            </span>
+        <div className='profile'>
+          <div className='profile-info'>
+            <div className='profile-info_avatar'>
+              <img
+                src={this.props.currentUser.avatar}
+                alt={this.props.currentUser.fullName}
+              />
+            </div>
+            <div className='profile-info_lines'>
+              <div className='profile-info_line'>
+                <span className='profile-info_line-subject'>Name: </span>
+                <span className='profile-info_line-info'>
+                  {this.props.currentUser.fullName
+                    ? this.props.currentUser.fullName
+                    : this.addName()}
+                </span>
+              </div>
+              <div className='profile-info_line'>
+                <span className='profile-info_line-subject'>Email: </span>
+                <span className='profile-info_line-info'>
+                  {this.props.currentUser.email
+                    ? this.props.currentUser.email
+                    : this.addEmail()}
+                </span>
+              </div>
+              <div className='profile-info_line'>
+                <span className='profile-info_line-subject'>Orders: </span>
+                <span className='profile-info_line-info'>
+                  {this.props.currentUser.orders
+                    ? this.props.currentUser.orders
+                    : 'No orders'}
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
